61b2054c12773a04c231757ec286f2f67ca386ab
WinterABW <skarnanotherblackwinter@gmail.com>
2026-04-11 20:53:33 -0400

    fix: remove /api prefix from apiBaseUrl in development environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-   apiBaseUrl: 'https://fifabanket.shop/api/',
+   apiBaseUrl: 'https://fifabanket.shop/',
   apiTimeout: 30000,
   tapSecretKey: 'MiClaveSecreta123!',
   cryptoDepositAddress: '',
